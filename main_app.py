7e1203988fde496082688694330390beda08e00d
annaliuu <liuanna2002@gmail.com>
2023-12-05 15:08:56 -0800

    fixed q&a bank

--- main_app.py
+++ main_app.py
@@ -6,15 +6,14 @@ import streamlit.config
 from process_materials import QuestionBank, process_lecture_materials
 
 # Initialize the question bank
-# Replace with your actual API key
-api_key = "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"
+api_key = "insert_api_key"
 lecture_materials = process_lecture_materials()
 question_bank = QuestionBank(api_key, lecture_materials)
 
 
 def main():
 
-    # Function to set the background
+    # Function to set custom background
     def set_bg_img(image_file):
         with open(image_file, "rb") as file:
             base64_img = base64.b64encode(file.read()).decode()
@@ -38,45 +37,62 @@ def main():
     # Select a topic
     topic = st.selectbox("Choose a topic:", list(lecture_materials.keys()))
 
+    if 'current_question' not in st.session_state:
+        st.session_state.current_question = None
+        st.session_state.summary = None
+        st.session_state.correct_answer = None
+        st.session_state.question_counter = 0  # Initialize the counter
+
     if st.button("Generate Question"):
         # Generate a question for the selected topic
-        question, summary = question_bank.generate_question(topic)
-        st.text_area("Question", value=question, height=100, disabled=True)
+        st.session_state.current_question, st.session_state.summary = question_bank.generate_question(topic)
+        st.session_state.correct_answer = None
+        st.session_state.question_counter += 1  # Increment the counter
 
-    # Answer input
-    user_answer = st.text_input("Your answer:")
-    question, summary = question_bank.generate_question(topic)
+    if st.session_state.current_question:
+        st.text_area("Question", value=st.session_state.current_question, height=100, disabled=True)
 
-    if user_answer:
-        # Assuming generate_answer returns the correct answer for the question
-        correct_answer = question_bank.generate_answer(question, summary)
+    # Answer input with counter as the key
+    user_answer = st.text_input("Your answer:", key=f"answer_input_{st.session_state.question_counter}")
 
+    if user_answer and not st.session_state.correct_answer:
+        # Generate answer only once
+        st.session_state.correct_answer = question_bank.generate_answer(st.session_state.current_question, st.session_state.summary)
+
+    if user_answer and st.session_state.correct_answer:
         # Compare user answer with the correct answer
-        if user_answer.lower().strip() == correct_answer.lower().strip():
-            #question_bank.update_performance(topic, correct=True)
+        if user_answer.lower().strip() == st.session_state.correct_answer[0].lower().strip():
             st.success("Correct!")
         else:
-            #question_bank.update_performance(topic, correct=False)
-            st.error(f"Incorrect. The correct answer is: {correct_answer}")
+            st.error(f"Incorrect. The correct answer is: {st.session_state.correct_answer}")
 
-    # Check the answer (Placeholder for actual logic)
-    # if user_answer:
-    #     correct_answer = question_bank.generate_answer(
-    #         question, summary)  # Placeholder
 
-    #     st.text_area("test", value=correct_answer)
+    # if 'current_question' not in st.session_state:
+    #     st.session_state.current_question = None
+    #     st.session_state.summary = None
+    #     st.session_state.correct_answer = None
 
-    #     if user_answer.lower() == correct_answer.lower():
-    #         question_bank.update_performance(topic, correct=True)
+    # if st.button("Generate Question"):
+    #     # Generate a question for the selected topic
+    #     st.session_state.current_question, st.session_state.summary = question_bank.generate_question(topic)
+    #     st.session_state.correct_answer = None  # Reset correct answer
+
+    # if st.session_state.current_question:
+    #     st.text_area("Question", value=st.session_state.current_question, height=100, disabled=True)
+
+    # # Answer input
+    # user_answer = st.text_input("Your answer:")
+
+    # if user_answer and not st.session_state.correct_answer:
+    #     # Generate answer only once
+    #     st.session_state.correct_answer = question_bank.generate_answer(st.session_state.current_question, st.session_state.summary)
+
+    # if user_answer and st.session_state.correct_answer:
+    #     # Compare user answer with the correct answer
+    #     if user_answer.lower().strip() == st.session_state.correct_answer.lower().strip():
     #         st.success("Correct!")
-    #         st.text_area("Answer", value=correct_answer,
-    #                      height=100, disabled=True)
     #     else:
-    #         question_bank.update_performance(topic, correct=False)
-    #         st.error(f"Incorrect. The correct answer is: {correct_answer}")
-    #         st.text_area("Answer", value=correct_answer,
-    #                      height=100, disabled=True)
-
+    #         st.error(f"Incorrect. The correct answer is: {st.session_state.correct_answer}")
 
 if __name__ == "__main__":
     main()
--- process_materials.py
+++ process_materials.py
@@ -4,7 +4,6 @@ import random
 
 # Part 1: Data Processing
 
-
 def read_markdown_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -14,11 +13,15 @@ def read_markdown_file(file_path):
         print(f"File not found: {file_path}")
         return None
 
-
 def process_lecture_materials(directory='lectures'):
     topics = {
-        "Introducing Language and Dialect": "1_intro_to_linguistics.mov.wav.txt",
-        # ... other topics and their corresponding filenames
+        "What is Language": "2_what_is_language.mp4.wav.txt",
+        "Phonetics": "4_phonetics_1.mp4.wav.txt",
+        "Phonology": "6_phonology_1.mp4.wav.txt",
+        "Morphology": "8_morphology.mp4.wav.txt",
+        "Syntax": "10_syntax1.mov.wav.txt",
+        "Semantics/Prgamatics": "14_semantics_and_pragmatics.mp4.wav.txt",
+        "Language Families": "16_language_families.mp4.wav.txt"
     }
 
     processed_materials = {}
@@ -30,14 +33,15 @@ def process_lecture_materials(directory='lectures'):
 
     return processed_materials
 
+
 # Part 2: Question Generation
-
-
 class QuestionBank:
     def __init__(self, api_key, lecture_materials):
         self.api_key = api_key
         self.lecture_materials = lecture_materials
 
+    # Code to summarize the entire lesson
+    # Not used since it heavily uses the API
     def summarize_chunk(self, chunk):
         try:
             response = openai.ChatCompletion.create(
@@ -56,11 +60,14 @@ class QuestionBank:
 
     def generate_question(self, topic):
         max_length = 3000
-        long_text = lecture_materials['Introducing Language and Dialect']
-        chunks = [long_text[i:i + max_length]
-                  for i in range(0, len(long_text), max_length)]
-        sum_chunks = [str(self.summarize_chunk(chunk)) for chunk in chunks]
-        summary = ''.join(sum_chunks)
+        long_text = lecture_materials[topic]
+
+        # Alternative usage using the API summary; not being used
+        # chunks = [long_text[i:i + max_length]
+        #           for i in range(0, len(long_text), max_length)]
+        # sum_chunks = [str(self.summarize_chunk(chunk)) for chunk in chunks]
+        # summary = ''.join(sum_chunks)
+        summary = long_text[:max_length]
 
         try:
             response = openai.ChatCompletion.create(
@@ -83,9 +90,6 @@ class QuestionBank:
             model="gpt-3.5-turbo",  # Specify the engine you want to use
             messages=[{"role": "system", "content": "This is a question and answer session. Answer the question based on the provided summary."},
                       {"role": "user", "content": f"What is the correct answer to {question} based on {summary}? Respond with exactly one letter, and the explanation."}],
-            # messages=f"What is the correct answer to {question} based on {summary}? Respond with exactly one letter, and the explanation.\n "
-            # "Follow this example format: "
-            # "A. Avocado. This is the answer because of blank",
             max_tokens=150,  # The maximum number of tokens to generate in the response
             api_key=api_key
         )
@@ -94,14 +98,14 @@ class QuestionBank:
 
 # Example usage
 # Replace with your actual API key
-api_key = "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"
+api_key = "insert_api_key"
 lecture_materials = process_lecture_materials()
 question_bank = QuestionBank(api_key, lecture_materials)
 
 # Generate a question based on performance
-next_question = question_bank.generate_question(
-    'Introducing Language and Dialect')
-print(next_question[0])
+# next_question = question_bank.generate_question(
+#     'Introducing Language and Dialect')
+# print(next_question[0])
 
-ans = question_bank.generate_answer(next_question[0], next_question[1])
-print(ans)
+# ans = question_bank.generate_answer(next_question[0], next_question[1])
+# print(ans)
